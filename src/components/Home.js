fb9c472084d44b08cf653a56fe812953d8bad3b3
Satya-Karthik-Palani <karthik.palani0827@gmail.com>
2024-04-01 13:50:16 +0530

    Responsive Nav Bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,25 +3,28 @@ import React from 'react'
 function Home() {
   return (
     <div className=''>
-        <div className='flex justify-between p-2 m-2'>
+        <div className='flex justify-between items-center p-2 m-2 lg:mx-20 lg:justify-start'>
           <div className='w-1/4'>
             <h1 className='text-red-600 font-bold'>BookUsNow</h1>
           </div>
           <div className='flex justify-end items-center w-3/5'>
-            <div className='mx-2 flex justify-between'>
-              <button className='hidden md:block'> ☰ Categories</button>
-              <form class="flex relative">
-                  <input type="text" class="hidden md:block border border-gray-500 rounded-md"/>
-                  <button class="block absolute right-1 top-1">
-                      <img src="./Images/search.svg" width="18px" height="18px" alt="Search Icon"/>
+            <div className='mx-2 flex justify-between lg:w-3/5'>
+              <button className='hidden lg:block bg-black text-white rounded-md p-1'> ☰ Categories</button>
+              <form className="flex relative w-4/6">
+                  <input type="text" className="hidden lg:block border border-gray-500 rounded-md w-full"/>
+                  <button className="block w-7 h-7 lg:absolute lg:right-1 lg:top-1 ">
+                      <img src="./Images/search.svg" alt="Search Icon"/>
                   </button>
               </form>
             </div>
             <div className='mx-2'>
-              <button><img src='./Images/heart.svg' width='18px' height='18px' alt='favorites icon'></img></button>
+              <button className='flex justify-center items-center'><img src='./Images/heart.svg' width='18px' height='18px' alt='favorites icon'></img><h1 className='hidden lg:block'>&nbsp;&nbsp;Favorites</h1></button>
             </div>
             <div className='mx-2'>
-              <button><img src='./Images/user.svg' width='18px' height='18px' alt='user icon'></img></button>
+              <button className='none lg:border border-[#BOBABF] p-1 rounded-lg'>
+                <img src='./Images/user.svg' width='18px' height='18px' alt='user icon' className='block lg:hidden'></img>
+                <h1 className='hidden lg:block'>Sign In</h1>
+              </button>
             </div>
           </div>
         </div>
