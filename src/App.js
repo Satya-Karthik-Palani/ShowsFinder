67f767c411ac13b112c8900767da20442f8428d2
Satya-Karthik-Palani <karthik.palani0827@gmail.com>
2024-04-02 20:15:59 +0530

    minor css changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
+
+import { Helmet } from 'react-helmet';import Home from './components/Home';
 
 function App() {
   return (
     <div className="bg-[#ffffff]">
       <Router>
+        <Helmet>
+          <title>Shows Finder</title> 
+        </Helmet>
         <Routes>
           <Route exact path='/' element={<Home/>}></Route>
         </Routes>
