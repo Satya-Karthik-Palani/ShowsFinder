d80f8f21cf790b00084a1580956fa28aae6e2c92
Satya-Karthik-Palani <karthik.palani0827@gmail.com>
2024-04-03 12:38:20 +0530

    Adapting to mobile screen size

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,9 +2,9 @@ import React from 'react'
 import moment from 'moment';
 function Event({e,id,weather}) {
   return (
-    <div className="w-80 sm:w-96 h-[350px] bg-[#ffffff] border border-[#BOBABF] rounded-lg mx-auto sm:mx-4 relative">
+    <div className="w-80 sm:w-96 h-[300px] sm:h-[350px] bg-[#ffffff] border border-[#BOBABF] rounded-lg mx-auto sm:mx-4 relative">
         <img src={`https://drive.google.com/thumbnail?id=${id}&sz=w1000`} alt="img"></img>
-        <div className='absolute top-[230px] rounded-t-none rounded-b-lg left-[4.5%] text-white w-[91%] bg-[rgba(49,49,49,0.8)] p-1'>
+        <div className='absolute top-[187px] rounded-t-none rounded-b-lg left-[4.5%] text-white w-[91%] bg-[rgba(49,49,49,0.8)] p-1'>
           <p className='ml-2'>{moment(e?.date).format('MMMM Do YYYY')}</p>
         </div>
         <div className='mx-7 my-2'>
